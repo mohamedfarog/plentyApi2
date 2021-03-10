abb428b7b5948939d6e7b3d97fbdcc61f2bd1613
Faisal <faisalkc4u@gmail.com>
2021-03-10 15:55:08 +0400

    changes

---
+++ database/migrations/2021_03_10_115338_changelength_logs.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangelengthLogs extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('logs', function (Blueprint $table) {
+            //
+            $table->string('message',65535)->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('logs', function (Blueprint $table) {
+            //
+        });
+    }
+}
