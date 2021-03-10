df814a3fb7693ceb9eca6bf760de31129502020c
Faisal <faisalkc4u@gmail.com>
2021-03-10 10:49:54 +0400

    changes

---
+++ resources/views/support1111.blade1.php
@@ -0,0 +1,9 @@
+Hello ,
+<br>
+<p> You've received a request from {{$data->user->name}} </p>
+<p>The details are as follows: </p>
+<p><b>Email Address:</b> &nbsp; {{$data->user->email}}</p>
+<p><b>Phone Number:</b> &nbsp; {{$data->user->contact}}</p>
+<p><b>Type:</b> &nbsp; {{$data->type}}</p>
+<p><b>Priority:</b> &nbsp; {{$data->priority}}</p>
+<p> <b>Description:</b> &nbsp; {{$data->description}}</p>
