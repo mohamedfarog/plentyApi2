fb9991b20745bfa40891f7d6e2ff69aea43cdd5f
Gillian Francisco <gillianfrancisco@gmail.com>
2021-03-23 19:15:46 +0400

    plenty website (init)

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,322 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Plenty of Things</title>
+
+    <meta charset="utf-8">
+    <!--[if IE]><meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'><![endif]-->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta name="description" content="">
+
+    <!-- Google Fonts -->
+    <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700%7COpen+Sans:400,400i,600,700'
+        rel='stylesheet'>
+
+    <!-- Css -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/magnific-popup.css" />
+    <link rel="stylesheet" href="css/font-icons.css" />
+    <link rel="stylesheet" href="css/sliders.css" />
+    <link rel="stylesheet" href="css/style.css" />
+    <link href='https://fonts.googleapis.com/css?family=Lato:400,700,900' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Bree+Serif' rel='stylesheet' type='text/css'>
+
+    <!-- all css here -->
+    <!-- bootstrap v3.3.6 css -->
+    <!-- animate css -->
+    <link rel="stylesheet" href="css/animate.css">
+    <!-- jquery-ui.min css -->
+    <link rel="stylesheet" href="css/jquery-ui.min.css">
+    <!-- meanmenu css -->
+    <link rel="stylesheet" href="css/meanmenu.min.css">
+    <!-- nivo-slider css -->
+    <link rel="stylesheet" href="lib/css/nivo-slider.css">
+    <link rel="stylesheet" href="lib/css/preview.css">
+    <!-- slick css -->
+    <link rel="stylesheet" href="css/slick.css">
+    <!-- lightbox css -->
+    <link rel="stylesheet" href="css/lightbox.min.css">
+    <!-- material-design-iconic-font css -->
+    <link rel="stylesheet" href="css/material-design-iconic-font.css">
+    <!-- All common css of theme -->
+    <link rel="stylesheet" href="css/default.css">
+    <!-- style css -->
+    <link rel="stylesheet" href="style.css">
+    <!-- shortcode css -->
+    <link rel="stylesheet" href="css/shortcode.css">
+    <!-- responsive css -->
+    <link rel="stylesheet" href="css/responsive.css">
+    <!-- modernizr css -->
+    <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+    <!-- Favicons -->
+    <link rel="shortcut icon" href="img/favicon.ico">
+    <link rel="apple-touch-icon" href="img/apple-touch-icon.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="img/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="img/apple-touch-icon-114x114.png">
+    <script type="text/javascript" src="js/jquery.min.js"></script>
+</head>
+
+<body class="relative">
+
+    <!-- Preloader -->
+    <div class="loader-mask">
+        <div class="loader">
+            <div></div>
+            <div></div>
+        </div>
+    </div>
+
+    <main class="main-wrapper">
+
+        <header class="nav-type-1">
+
+            <!-- Fullscreen search -->
+            <div class="search-wrap">
+                <div class="search-inner">
+                    <div class="search-cell">
+                        <form method="get">
+                            <div class="search-field-holder">
+                                <input type="search" class="form-control main-search-input" placeholder="Search for">
+                                <i class="ui-close search-close" id="search-close"></i>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div> <!-- end fullscreen search -->
+
+
+
+            <nav class="navbar navbar-static-top">
+                <div class="navigation" id="sticky-nav">
+                    <div class="container relative">
+
+                        <div class="row flex-parent">
+
+                            <div class="navbar-header flex-child flex-right">
+                                <!-- Logo -->
+                                <div class="logo-container">
+                                    <div class="logo-wrap">
+                                        <a href="index.html">
+                                            <img class="logo-dark" src="img/logo_dark.png" alt="logo"
+                                                style="max-height:80%;">
+                                        </a>
+                                    </div>
+                                </div>
+                                <button type="button" class="navbar-toggle" data-toggle="collapse"
+                                    data-target="#navbar-collapse">
+                                    <span class="sr-only">Toggle navigation</span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                </button>
+                                <!-- Mobile cart -->
+                                <div class="nav-cart mobile-cart hidden-lg hidden-md">
+                                    <div class="nav-cart-outer">
+                                        <div class="nav-cart-inner">
+                                            <a href="#" class="nav-cart-icon">
+                                                <span class="nav-cart-badge">2</span>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div> <!-- end navbar-header -->
+
+                            <div class="nav-wrap flex-child" style="flex: 3 0 0;">
+                                <div class="collapse navbar-collapse text-center" id="navbar-collapse">
+
+                                    <ul class="nav navbar-nav">
+
+                                        <li class="">
+                                            <a href="#">Home</a> 
+                                        </li>
+ 
+                                        <li class="">
+                                            <a href="#">Delicacy</a>
+                                    
+                                        </li> <!-- end elements -->
+                                        <li class="">
+                                            <a href="#">Beauty</a>
+                                           
+                                        </li> <!-- end elements --> 
+                                        <li class="">
+                                            <a href="#">Fashion</a>
+                                           
+                                        </li> <!-- end elements -->
+                                        <li class="">
+                                            <a href="#">Featured</a>
+                                            
+                                        </li> <!-- end elements -->
+                                        <li class="">
+                                            <a href="#">Brands</a>
+                                            
+                                        </li> <!-- end elements -->
+   
+                                        <!-- Mobile search -->
+                                        <li id="mobile-search" class="hidden-lg hidden-md">
+                                            <form method="get" class="mobile-search">
+                                                <input type="search" class="form-control" placeholder="Search...">
+                                                <button type="submit" class="search-button">
+                                                    <i class="fa fa-search"></i>
+                                                </button>
+                                            </form>
+                                        </li>
+
+                                    </ul> <!-- end menu -->
+                                </div> <!-- end collapse -->
+                            </div> <!-- end col -->
+
+                            <div class="flex-child flex-right nav-right hidden-sm hidden-xs" style="padding:0">
+                                <ul>
+
+                                    <li class="nav-search-wrap style-2 hidden-sm hidden-xs">
+                                        <a href="#" class="nav-search search-trigger">
+                                            <img src="img/nav/search.png" style="width:50px;height:50px;">
+                                        </a>
+                                    </li>
+                                    <li class="nav-cart">
+                                        <div class="nav-cart-outer">
+                                            <div class="nav-cart-inner">
+                                                <a href="#" class="nav-cart-icon">
+
+                                                </a>
+                                            </div>
+                                        </div>
+                                        <!-- <div class="nav-cart-container">
+                                            <div class="nav-cart-items">
+
+                                                <div class="nav-cart-item clearfix">
+                                                    <div class="nav-cart-img">
+                                                        <a href="#">
+                                                            <img src="img/shop/shop_item_1.jpg" alt="">
+                                                        </a>
+                                                    </div>
+                                                    <div class="nav-cart-title">
+                                                        <a href="#">
+                                                            Ladies Bag
+                                                        </a>
+                                                        <div class="nav-cart-price">
+                                                            <span>1 x</span>
+                                                            <span>1250.00</span>
+                                                        </div>
+                                                    </div>
+                                                    <div class="nav-cart-remove">
+                                                        <a href="#" class="remove"><i class="ui-close"></i></a>
+                                                    </div>
+                                                </div>
+
+                                                <div class="nav-cart-item clearfix">
+                                                    <div class="nav-cart-img">
+                                                        <a href="#">
+                                                            <img src="img/shop/shop_item_2.jpg" alt="">
+                                                        </a>
+                                                    </div>
+                                                    <div class="nav-cart-title">
+                                                        <a href="#">
+                                                            Sequin Suit longer title
+                                                        </a>
+                                                        <div class="nav-cart-price">
+                                                            <span>1 x</span>
+                                                            <span>1250.00</span>
+                                                        </div>
+                                                    </div>
+                                                    <div class="nav-cart-remove">
+                                                        <a href="#" class="remove"><i class="ui-close"></i></a>
+                                                    </div>
+                                                </div>
+
+                                            </div>  
+                                            <div class="nav-cart-summary">
+                                                <span>Cart Subtotal</span>
+                                                <span class="total-price">$1799.00</span>
+                                            </div>
+
+                                            <div class="nav-cart-actions mt-20">
+                                                <a href="shop-cart.html" class="btn btn-md btn-dark"><span>View
+                                                        Cart</span></a>
+                                                <a href="shop-checkout.html"
+                                                    class="btn btn-md btn-color mt-10"><span>Proceed to
+                                                        Checkout</span></a>
+                                            </div>
+                                        </div> -->
+                                    </li>
+                                    <li class="nav-search-wrap style-2 hidden-sm hidden-xs">
+                                        <a href="#" class="nav-search search-trigger">
+                                            <i class="fa fa-heart"></i>
+                                        </a>
+                                    </li>
+                                    <li class="nav-search-wrap style-2 hidden-sm hidden-xs">
+                                        <a href="#" class="nav-search search-trigger">
+                                            <i class="fa fa-user"></i>
+                                        </a>
+                                    </li>
+                                    <li class="nav-search-wrap style-2 hidden-sm hidden-xs">
+                                        <a href="#" class="nav-search search-trigger">
+                                            <i class="fa fa-globe"></i>
+                                           
+                                        </a>
+                                    </li>
+                                    <li class="dropdown nav-search-wrap style-2 hidden-sm hidden-xs">
+                                    <a href="#" class="menufont">ENG</a>
+                                      
+                                      <ul class="dropdown-menu">
+                                      
+                                        <li><a href="index-2.html">Arabic</a></li> 
+                                      </ul>
+                                      <li class="">
+                                          <i class="fa fa-angle-down dropdown-trigger"></i> 
+                                    </li>
+                                    </li>
+
+                                </ul>
+                            </div>
+
+                        </div> <!-- end row -->
+                    </div> <!-- end container -->
+                </div> <!-- end navigation -->
+            </nav> <!-- end navbar -->
+        </header>
+
+
+
+        <div class="content-wrapper oh">
+
+            @yield('content')
+
+        </div> <!-- end content wrapper -->
+    </main> <!-- end main wrapper -->
+
+    <!-- jQuery Scripts -->
+
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/plugins.js"></script>
+    <script type="text/javascript" src="js/scripts.js"></script>
+
+    <!-- bootstrap js -->
+    <!-- jquery.meanmenu js -->
+    <script src="js/jquery.meanmenu.js"></script>
+    <!-- slick.min js -->
+    <script src="js/slick.min.js"></script>
+    <!-- jquery.treeview js -->
+    <script src="js/jquery.treeview.js"></script>
+    <!-- lightbox.min js -->
+    <script src="js/lightbox.min.js"></script>
+    <!-- jquery-ui js -->
+    <script src="js/jquery-ui.min.js"></script>
+    <!-- jquery.nivo.slider js -->
+    <script src="lib/js/jquery.nivo.slider.js"></script>
+    <script src="lib/home.js"></script>
+    <!-- jquery.nicescroll.min js -->
+    <script src="js/jquery.nicescroll.min.js"></script>
+    <!-- countdon.min js -->
+    <script src="js/countdon.min.js"></script>
+    <!-- wow js -->
+    <script src="js/wow.min.js"></script>
+    <!-- plugins js -->
+    <script src="js/plugins.js"></script>
+    <!-- main js -->
+    <script src="js/main.js"></script>
+
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,36 +16,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/supportimages/{filename}', function($filename){
-    $path = resource_path() . '/assets/supportimages/' . $filename;
-
-    if(!File::exists($path)) {
-        return response()->json(['message' => 'Image not found.'], 404);
-    }
-
-    $file = File::get($path);
-    $type = File::mimeType($path);
-
-    $response = Response::make($file, 200);
-    $response->header("Content-Type", $type);
-
-    return $response;
+    return redirect('/home');
 });
 
-Route::get('/orderimages/{filename}', function($filename){
-    $path = resource_path() . '/assets/orderimages/' . $filename;
-
-    if(!File::exists($path)) {
-        return response()->json(['message' => 'Image not found.'], 404);
-    }
-
-    $file = File::get($path);
-    $type = File::mimeType($path);
-
-    $response = Response::make($file, 200);
-    $response->header("Content-Type", $type);
-
-    return $response;
+Route::get('/#', function () {
+    return redirect('/home');
 });
+
+
+Route::get('/home', function () {
+    return view('home');
+}); 
