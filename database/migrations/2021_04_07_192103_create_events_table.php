5315d1066fda98284ea1a6fa5b5f63b83e930727
Eric <faisalkc4u@gmail.com>
2021-04-07 19:27:21 +0400

    changes

---
+++ database/migrations/2021_04_07_192103_create_events_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('name_en');
+            $table->string('name_ar');
+            $table->string('desc_en');
+            $table->string('desc_ar');
+            $table->string('image');
+            $table->string('video');
+            $table->dateTime('start')->nullable();
+            $table->dateTime('end')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
