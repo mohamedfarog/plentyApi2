7e6e7f616f881bb4fe6798301740f9da9c167d78
Mohammed <mhdnasir44@gmail.com>
2021-04-11 12:46:52 +0400

    Changes

--- app/Http/Controllers/EventcatController.php
+++ app/Http/Controllers/EventcatController.php
@@ -15,8 +15,8 @@ class EventcatController extends Controller
      */
     public function index(Request $request)
     {
-        //
-        $shops = Shop::whereNotNull('eventcat_id', null)->get();
+  
+        $shops = Shop::with(['shop','style','designers'])->whereNotNull('eventcat_id', null)->get();
         if (isset($request->cat_id)) {
             $shops = Shop::where('eventcat_id', $request->cat_id)->get();
         }
