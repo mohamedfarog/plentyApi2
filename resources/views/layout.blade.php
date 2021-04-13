7958309f300fe4579dbc5b0010119349a1b27815
Gillian Francisco <gillianfrancisco@gmail.com>
2021-04-13 15:22:09 +0400

    no message

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -280,115 +280,61 @@
                                                 </a>
                                             </div>
                                         </div>
-                                        <div class="nav-cart-container">
+                                       <div class="nav-cart-container">
+                      <div class="nav-cart-items">
 
+                        <div class="nav-cart-item clearfix">
+                          <div class="nav-cart-img">
+                            <a href="#">
+                              <img src="img/shop/shop_item_1.jpg" alt="">
+                            </a>
+                          </div>
+                          <div class="nav-cart-title">
+                            <a href="#">
+                              Ladies Bag
+                            </a>
+                            <div class="nav-cart-price">
+                              <span>1 x</span>
+                              <span>1250.00</span>
+                            </div>
+                          </div>
+                          <div class="nav-cart-remove">
+                            <a href="#" class="remove"><i class="ui-close"></i></a>
+                          </div>
+                        </div>
 
+                        <div class="nav-cart-item clearfix">
+                          <div class="nav-cart-img">
+                            <a href="#">
+                              <img src="img/shop/shop_item_2.jpg" alt="">
+                            </a>
+                          </div>
+                          <div class="nav-cart-title">
+                            <a href="#">
+                              Sequin Suit longer title
+                            </a>
+                            <div class="nav-cart-price">
+                              <span>1 x</span>
+                              <span>1250.00</span>
+                            </div>
+                          </div>
+                          <div class="nav-cart-remove">
+                            <a href="#" class="remove"><i class="ui-close"></i></a>
+                          </div>
+                        </div>
 
-                                            <div class="nav-cart-items">
+                      </div> <!-- end cart items -->
 
-                                                <div class="nav-cart-item clearfix">
-                                                    <div class="nav-cart-img">
-                                                        <a href="#">
-                                                            <img src="img/shop/shop_item_1.jpg" alt="">
-                                                        </a>
-                                                    </div>
-                                                    <div class="nav-cart-title">
-                                                        <a href="#">
-                                                            Ladies Bag
-                                                        </a>
-                                                        <div class="nav-cart-price">
-                                                            <span>1 x</span>
-                                                            <span>1250.00</span>
-                                                        </div>
-                                                    </div>
-                                                    <div class="nav-cart-remove">
-                                                        <a href="#" class="remove"><i class="ui-close"></i></a>
+                      <div class="nav-cart-summary">
+                        <span>Cart Subtotal</span>
+                        <span class="total-price">$1799.00</span>
+                      </div>
 
-                                                        <div class="nav-cart-items">
-
-                                                            <div class="nav-cart-item clearfix">
-                                                                <div class="nav-cart-img">
-                                                                    <a href="#">
-                                                                        <img src="img/shop/shop_item_1.jpg" alt="">
-                                                                    </a>
-                                                                </div>
-                                                                <div class="nav-cart-title">
-                                                                    <a href="#">
-                                                                        Ladies Bag
-                                                                    </a>
-                                                                    <div class="nav-cart-price">
-                                                                        <span>1 x</span>
-                                                                        <span>1250.00</span>
-
-                                                                    </div>
-                                                                </div>
-
-                                                                <div class="nav-cart-item clearfix">
-                                                                    <div class="nav-cart-img">
-                                                                        <a href="#">
-                                                                            <img src="img/shop/shop_item_2.jpg" alt="">
-                                                                        </a>
-                                                                    </div>
-                                                                    <div class="nav-cart-title">
-                                                                        <a href="#">
-                                                                            Sequin Suit longer title
-                                                                        </a>
-                                                                        <div class="nav-cart-price">
-                                                                            <span>1 x</span>
-                                                                            <span>1250.00</span>
-                                                                        </div>
-                                                                    </div>
-                                                                    <div class="nav-cart-remove">
-                                                                        <a href="#" class="remove"><i class="ui-close"></i></a>
-                                                                    </div>
-                                                                </div>
-
-                                                            </div>
-                                                            <div class="nav-cart-summary">
-                                                                <span>Cart Subtotal</span>
-                                                                <span class="total-price">$1799.00</span>
-                                                            </div>
-
-                                                            <div class="nav-cart-actions mt-20">
-                                                                <a href="{{url('/cart')}}" class="btn btn-md btn-dark"><span>View
-                                                                        Cart</span></a>
-                                                                <a href="shop-checkout.html" class="btn btn-md btn-color mt-10"><span>Proceed to
-                                                                        Checkout</span></a>
-                                                            </div>
-
-                                                        </div>
-                                                        <div class="nav-cart-summary">
-                                                            <span>Cart Subtotal</span>
-                                                            <span class="total-price">$1799.00</span>
-                                                        </div>
-
-
-                                                    </div>
-                                                    <div class="nav-cart-summary">
-                                                        <span>Cart Subtotal</span>
-                                                        <span class="total-price">$1799.00</span>
-                                                    </div>
-
-                                                    <div class="nav-cart-actions mt-20">
-                                                        <a href="/cart" class="btn btn-md btn-dark"><span>View
-                                                                Cart</span></a>
-                                                        <a href="shop-checkout.html" class="btn btn-md btn-color mt-10"><span>Proceed to
-                                                                Checkout</span></a>
-                                                    </div>
-                                                </div>
-
-                                            </div> <!-- end cart items -->
-
-                                            <div class="nav-cart-summary">
-                                                <span>Cart Subtotal</span>
-                                                <span class="total-price">$1799.00</span>
-                                            </div>
-
-                                            <div class="nav-cart-actions mt-20">
-                                                <a href="/cart" class="btn btn-md btn-dark"><span>View Cart</span></a>
-                                                <a href="/checkout" class="btn btn-md btn-color mt-10"><span>Proceed to Checkout</span></a>
-                                            </div>
-                                        </div>
+                      <div class="nav-cart-actions mt-20">
+                        <a href="shop-cart.html" class="btn btn-md btn-dark"><span>View Cart</span></a>
+                        <a href="shop-checkout.html" class="btn btn-md btn-color mt-10"><span>Proceed to Checkout</span></a>
+                      </div>
+                    </div>
                                     </li>
                                     <li class="nav-search-wrap style-2 hidden-sm hidden-xs">
                                         <a href="/cart" class="nav-search  imgicon">
