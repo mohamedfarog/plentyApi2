38d57234f9e46026110c0a7e397f349045b1e8ea
Mohammed <mhdnasir44@gmail.com>
2021-04-19 13:19:27 +0400

    changes

--- app/Models/Eventcat.php
+++ app/Models/Eventcat.php
@@ -11,9 +11,19 @@ class Eventcat extends Model
     protected $fillable = [
         'name_en', 'name_ar', 'image', 'event_id',
     ];
+    protected $appends = [
+        'imgurl',
+        
+    ];
 
     public function shops()
     {
         return $this->hasMany(Shop::class);
+    }
+    public function getImgurlAttribute()
+    {
+        if ($this->image != null) {
+            return env('CATURL') . $this->image;
+        }
     }
 }
