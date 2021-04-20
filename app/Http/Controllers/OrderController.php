7e3e21d9121678a9bdaf98d0f662b22fc55b8039
Eric <faisalkc4u@gmail.com>
2021-04-20 15:47:12 +0400

    bug fixes

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -40,7 +40,7 @@ class OrderController extends Controller
                 $shop=171;
                 $q = Detail::join('products', "product_id", "products.id")->select('products.shop_id', 'products.price', 'details.*')->with(['order', 'product' => function ($product_info) use ($shop) {
                     return $product_info->with('images')->where('products.shop_id', '=', $shop);
-                }])->has('product.shop')->where('product.shop_id', '=', $shop);
+                }])->where('products.shop_id', '=', $shop);
                 if (isset($request->status))
                     $q->join('orders', "order_id", "orders.id")->where('status', '=', $request->status,);
                 return $q->paginate();
