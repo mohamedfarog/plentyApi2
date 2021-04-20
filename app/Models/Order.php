c56f50a7487add10a480864bb4fcc0bb133b6837
Eric <faisalkc4u@gmail.com>
2021-04-20 12:26:43 +0400

    added status values in order

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -9,9 +9,9 @@ class Order extends Model
 {
     use HasFactory;
     protected $fillable = [
-        'ref', 'total_amount', 'amount_due', 'order_status', 'payment_method', 'tax', 'delivery_charge', 'delivery_location', 'user_id','coupon_value','lat','lng','delivery_note','contact_number','city','label'
+        'ref', 'total_amount', 'amount_due', 'order_status', 'payment_method', 'tax', 'delivery_charge', 'delivery_location', 'user_id', 'coupon_value', 'lat', 'lng', 'delivery_note', 'contact_number', 'city', 'label'
     ];
-
+    protected $appends = ['orderstatusvalue'];
     public function details()
     {
         return $this->hasMany(Detail::class);
@@ -19,5 +19,29 @@ class Order extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+    public function getOrderstatusvalueAttribute()
+    {
+        switch ($this->order_status) {
+            case '0':
+                return "Confirmed";
+                break;
+            case '1':
+                return "Preparing";
+                break;
+            case '2':
+                return "Out for Delivery";
+                break;
+            case '3':
+                return "Delivered";
+                break;
+            case '4':
+                return "Rejected";
+                break;
+
+            default:
+                return "Unkown";
+                break;
+        }
     }
 }
