79b42157b84929f39aa5013d0d5ddfd3f8b9ed86
Eric <faisalkc4u@gmail.com>
2021-04-20 16:03:10 +0400

    test

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -37,17 +37,11 @@ class OrderController extends Controller
                 break;
             case 'V':
             case 'v':
-                $shop=171;
-                $q = Detail::join('products', "product_id", "products.id")->select('products.shop_id', 'products.price', 'details.*')->with(['order', 'product' => function ($product_info) use ($shop) {
-                    return $product_info->with('images')->where('products.shop_id', '=', $shop);
-                }])->where('products.shop_id', '=', $shop);
-                if (isset($request->status))
-                    $q->join('orders', "order_id", "orders.id")->where('status', '=', $request->status,);
-                return $q->unique("order_id");
-                
-              
-                return Order::whereIn('id',function($ordersInfo){
-                    
+                $shop = 171;
+                return Detail::select('order_id')->select()->distinct()->get();
+
+
+                return Order::whereIn('id', function ($ordersInfo) {
                 })->paginate();
                 break;
             case 'S':
