ec566ccf713d58fa59aa852a7895229d829946c6
Faisal KC <faisalkc4u@gmail.com>
2021-04-21 10:36:41 +0400

    bug fixes for shop

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -43,8 +43,8 @@ class OrderController extends Controller
                 $shop=ShopInfo::where('user_id',$user->id)->first();
                 if(!$shop)
                 return response()->json(['success' => false, 'message' => "You dont't have enough perimission to access the data", ],400);
-                $orders= Order::join('details', 'details.order_id', 'orders.id')->where('shop_id', $shop->id)->select("orders.*","details.shop_id")->with(['details' => function ($details) {
-                    return $details->with(['product' => function ($product) {
+                $orders= Order::join('details', 'details.order_id', 'orders.id')->where('shop_id', $shop->id)->select("orders.*","details.shop_id")->with(['details' => function ($details) use ($shop) {
+                    return $details->where('shop_id', $shop->id)->with(['product' => function ($product) {
                         return $product->with(['images']);
                     }, 'size', 'color']);
                 },]);
