121ead54138ebb987f96a8000beb82d25bb917fa
Mohammed <mhdnasir44@gmail.com>
2021-04-26 12:50:44 +0400

    Changes

---
+++ app/Http/Controllers/TableBookingDetailController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TableBookingDetail;
+use Illuminate\Http\Request;
+
+class TableBookingDetailController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\TableBookingDetail  $tableBookingDetail
+     * @return \Illuminate\Http\Response
+     */
+    public function show(TableBookingDetail $tableBookingDetail)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\TableBookingDetail  $tableBookingDetail
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(TableBookingDetail $tableBookingDetail)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\TableBookingDetail  $tableBookingDetail
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, TableBookingDetail $tableBookingDetail)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\TableBookingDetail  $tableBookingDetail
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(TableBookingDetail $tableBookingDetail)
+    {
+        //
+    }
+}
