d512a8e0cb26ff77a566cd3fba0bc550c4cb4aca
Mohammed <mhdnasir44@gmail.com>
2021-04-26 13:00:04 +0400

    Changes

---
+++ database/migrations/2021_04_26_125925_addfields_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableBookingDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('table_booking_details', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('tablebookingid')->nullable();
+            $table->foreign('tablebookingid')->references('id')->on('table_bookings');
+            $table->unsignedBigInteger('addonid')->nullable();
+            $table->foreign('addonid')->references('id')->on('addons');
+            $table->unsignedBigInteger('size_id')->nullable();
+            $table->foreign('size_id')->references('id')->on('sizes');
+            $table->integer('qty');
+            $table->unsignedBigInteger('shop_id')->nullable();
+            $table->foreign('shop_id')->references('id')->on('shops');
+            $table->double('price')->nullable();
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('table_booking_details');
+    }
+}
