1c04e3af93dae9fd2c819cad4f07e12f98c9b50d
Mohammed <mhdnasir44@gmail.com>
2021-04-27 11:01:15 +0400

    Changes

---
+++ database/migrations/2021_04_27_104731_create_sched_times_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSchedTimesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sched_times', function (Blueprint $table) {
+            $table->id();
+            $table->time('from')->nullable();
+            $table->time('to')->nullable();
+            $table->unsignedBigInteger('table_id')->nullable();
+            $table->foreign('table_id')->references('id')->on('shoptables');
+            $table->tinyInteger('status')->nullable();
+            $table->boolean('booked')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sched_times');
+    }
+}
