0eabe7642518fe01e4f9da836d39f809cdc42231
Mohammed <mhdnasir44@gmail.com>
2021-04-27 11:47:06 +0400

    Changes

--- app/Http/Controllers/SchedTimeController.php
+++ app/Http/Controllers/SchedTimeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\SchedTime;
+use App\Models\Tablesched;
 use Illuminate\Http\Request;
 
 class SchedTimeController extends Controller
@@ -35,7 +36,22 @@ class SchedTimeController extends Controller
      */
     public function store(Request $request)
     {
+
         $schedtime= new SchedTime();
+        $day= $request->day;
+        $istoday= $request->istoday;
+        //Find if the day is today
+        //If it is today, then we return the timeslots existing in the database
+        
+
+        if($istoday==true){
+
+        }
+        else{
+            $tablescheds= Tablesched::where('shop_id',$request->shop_id)->where('day',$request->day)->first();  
+            return $schedtime->generateTimeSlots ($tablescheds->opening,$tablescheds->closing,$tablescheds->seating_time,0);
+
+        }
         //Generate Time Slots for that ID
         return $schedtime->generateTimeSlots ('10:00:00','12:00:00',15,1);
     }
