dc66dcfdb6f564709abab0d2dcbaea5477c3abf4
Mohammed <mhdnasir44@gmail.com>
2021-04-28 13:40:28 +0400

    Changes

--- app/Http/Controllers/SchedTimeController.php
+++ app/Http/Controllers/SchedTimeController.php
@@ -39,7 +39,7 @@ class SchedTimeController extends Controller
      */
     public function store(Request $request)
     {
-        return date('YYYY-MM-DD', strtotime($request->date));
+        return date('Y-m-d', strtotime($request->date));
 
         $schedtime= new SchedTime();
         $day= $request->day;
