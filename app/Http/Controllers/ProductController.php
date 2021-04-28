a8054d4e923c5919771b76d62d0decaef6567419
Mohammed <mhdnasir44@gmail.com>
2021-04-28 16:21:09 +0400

    Changes

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -491,7 +491,11 @@ class ProductController extends Controller
             $arr= array();
             // This is used for fetch products for array
             foreach($request->products as $requestproduct){
-                    array_push($arr,$product->where("id", $requestproduct)->first()->id);
+                $p= $product->where("id", $requestproduct)->first()->id;
+                if($p){
+                    array_push($arr,$p);
+                }
+                    
             }
             return $arr;
             $product = $product->whereIn("id", $request->products);
