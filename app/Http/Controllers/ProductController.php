6084ec3af7820322824a4ffd8da024b317d48140
Mohammed <mhdnasir44@gmail.com>
2021-04-29 11:11:37 +0400

    Changes

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -32,7 +32,7 @@ class ProductController extends Controller
                     $perpage = $request->perpage;
                 }
                 if (isset($request->all)) {
-                    return Product::where('deleted_at',null)->with(['sizes', 'colors', 'addons', 'images', 'designer'])->all();
+                    return Product::where('deleted_at',null)->with(['sizes', 'colors', 'addons', 'images', 'designer'])->get();
                 }
                 if(isset($request->eventcat_id))
                 return Product::where('deleted_at',null)->where('eventcat_id',$request->eventcat_id)->with(['sizes', 'colors', 'addons', 'images', 'designer'])->paginate($perpage);
