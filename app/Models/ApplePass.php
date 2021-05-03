ef012207aa62bd69ca20fee8f061d5bff154315a
Eric <faisalkc4u@gmail.com>
2021-05-03 13:28:26 +0400

    changes

--- app/Models/ApplePass.php
+++ app/Models/ApplePass.php
@@ -32,12 +32,12 @@ class ApplePass extends Model
             "backgroundColor"   => $project->loyaltybcolor,
             "labelColor" => $project->loyaltyfcolor,
             "barcode" => [
-                "message"   => [
-                    "customer_name" => $user->name,
-                    "customer_mobile_number" => $user->contact,
-                    "mobile_country_code" => substr($user->contact, 0, 4),
-                    "reward_code" => $pass_identifier
-                ],
+                "message"   => '{
+                    "customer_name": ' . $user->name . ',
+                    "customer_mobile_number": ' . $user->contact . ',
+                    "mobile_country_code": ' . substr($user->contact, 0, 4) . ',
+                    "reward_code": ' . $pass_identifier . '
+                  }',
                 "format"    => $project->barcodeformat,
                 "altText"   => $pass_identifier,
                 "messageEncoding" => $project->barcodemsgencoding,
@@ -144,12 +144,7 @@ class ApplePass extends Model
             "backgroundColor"   => $project->accessbcolor,
             "labelColor" => $project->accessfcolor,
             "barcode" => [
-                "message"   => [
-                    "customer_name" => $user->name,
-                    "customer_mobile_number" => $user->contact,
-                    "mobile_country_code" => substr($user->contact, 0, 4),
-                    "reward_code" => $pass_identifier
-                ],
+                "message"   => $pass_identifier,
                 "format"    => $project->barcodeformat,
                 "altText"   => $pass_identifier,
                 "messageEncoding" => $project->barcodemsgencoding,
