7bf864373ccb290ecd91c84ac17ccf005e8e2071
Eric <faisalkc4u@gmail.com>
2021-05-03 16:52:52 +0400

    test

--- app/Http/Middleware/passMiddleware.php
+++ app/Http/Middleware/passMiddleware.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class passMiddleware
 {
@@ -16,7 +17,8 @@ class passMiddleware
      */
     public function handle(Request $request, Closure $next)
     {
-
+        Log::info("Apple pass middleware");
+        Log::info($request->header('Authorization'));
         if ($request->header('Authorization') != null) {
             $key = explode(' ', $request->header('Authorization'));
             if (in_array($key[1], ['NgBeW4dQFsiGiTiDUOlcam6H', 'JEvmfd9hfiURvdqXMYE39r4E', 'eT7JV88G9El3888Eu6gnoGFB'])) {
