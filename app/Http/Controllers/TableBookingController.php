38947b70eab27ec5614404a10482d0f9bb6dece6
Mohammed <mhdnasir44@gmail.com>
2021-05-05 13:22:18 +0400

    Minor Changes

--- app/Http/Controllers/TableBookingController.php
+++ app/Http/Controllers/TableBookingController.php
@@ -61,7 +61,7 @@ class TableBookingController extends Controller
             }
             if($request->action=='pickup'){
                 $orders = TableBooking::whereNull('table_id')->with(['details' => function ($details) use ($shopid,$dt) {
-                    return $details->with(['product','color','addons'])->where('shop_id', $shopid)->whereDate('created_at', '=', $dt->toDateString());
+                    return $details->with(['product','size','color','addons'])->where('shop_id', $shopid)->whereDate('created_at', '=', $dt->toDateString());
                 }, 'user'])->get();
             }
             // $orders = TableBooking::with(['details' => function ($details) use ($shopid, $dt) {
