c9d18fd6aa64c9c3f1efc056e280066925e8d4fd
Eric <faisalkc4u@gmail.com>
2021-05-05 10:39:46 +0400

    changes

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -77,6 +77,7 @@ class OrderController extends Controller
                         return $product->with(['images']);
                     }, 'size', 'color']);
                 },]);
+                return $orders;
 
                 break;
             case 'S':
