910e4c8e3a7ac79cc8191440f2b719637ee48d85
Mohammed <mhdnasir44@gmail.com>
2021-05-06 17:20:55 +0400

    c

--- app/Models/Coupon.php
+++ app/Models/Coupon.php
@@ -7,10 +7,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Coupon extends Model
 {   
+    protected $appends = ['expired'];
     public function shop()
     {
         return $this->hasOne(Shop::class,'id','shop_id');
     }
+    public function getExpiredAttribute()
+    {
+        if ($this->expiry != null) {
+            if(now()->gte($this->expiry)) {
+                return 'Expired';
+            }
+            else{
+                return $this->expiry->format('d M Y');
+            }
+        }
+    }
+
+    
 
 
     use HasFactory;
