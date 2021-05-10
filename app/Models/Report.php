0af7223bab3d69c0eee4debf531cc3820532ae87
Eric <faisalkc4u@gmail.com>
2021-05-10 13:31:32 +0400

    changes

--- app/Models/Report.php
+++ app/Models/Report.php
@@ -96,7 +96,7 @@ class Report extends Model
                     break;
                     case 'shopearnings':
                         $brandsid = Shop::whereNotNull('cat_id')->where('active',1)->pluck('id','name_en')->toArray();
-
+return $brandsid;
                         foreach ($brandsid as $brand) {
                             $data[$brand['name_en']] = floatval(Order::join('details','orders.id','=','details.order_id')->where('order_status', 3)->sum('details.price'));
                         }
