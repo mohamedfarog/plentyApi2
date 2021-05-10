336e0d4f2954c147184f8382b0c26d5578b9c312
Eric <faisalkc4u@gmail.com>
2021-05-10 16:54:55 +0400

    changes

--- app/Http/Controllers/SchedTimeController.php
+++ app/Http/Controllers/SchedTimeController.php
@@ -144,7 +144,9 @@ class SchedTimeController extends Controller
                         }
                     }
                 }
-                return $array;
+
+
+                return $this->array_sort($array,'fromtime');
             } else {
                 return response()->json(['Error' => 'No tables available'], 400);
             }
@@ -188,6 +190,41 @@ class SchedTimeController extends Controller
         }
     }
 
+
+    function array_sort($array, $on, $order=SORT_ASC)
+{
+    $new_array = array();
+    $sortable_array = array();
+
+    if (count($array) > 0) {
+        foreach ($array as $k => $v) {
+            if (is_array($v)) {
+                foreach ($v as $k2 => $v2) {
+                    if ($k2 == $on) {
+                        $sortable_array[$k] = $v2;
+                    }
+                }
+            } else {
+                $sortable_array[$k] = $v;
+            }
+        }
+
+        switch ($order) {
+            case SORT_ASC:
+                asort($sortable_array);
+            break;
+            case SORT_DESC:
+                arsort($sortable_array);
+            break;
+        }
+
+        foreach ($sortable_array as $k => $v) {
+            $new_array[$k] = $array[$k];
+        }
+    }
+
+    return $new_array;
+}
 
 
 
