bc5eeac7d99bbc7988cfbc0139e7036889486f7b
Eric <faisalkc4u@gmail.com>
2021-05-10 15:05:36 +0400

    changes

--- app/Http/Controllers/SchedTimeController.php
+++ app/Http/Controllers/SchedTimeController.php
@@ -113,12 +113,13 @@ class SchedTimeController extends Controller
                 $array = array();
                 foreach ($tables as $table) {
                     $timeslots = SchedTime::where('table_id', $table->id)->where('booked', 0)->get();
-                    foreach($timeslots as $timeslot){
-                        if (!in_array($array, ['fromtime' => $timeslot->from, 'totime' => $timeslot->to, 'table_id'=>$timeslot->table_id, 'booked'=>0])) {
-                            array_push($array, ['fromtime' => $timeslot->from, 'totime' => $timeslot->to, 'table_id'=>$timeslot->table_id,'booked'=>0]);
+                    foreach ($timeslots as $timeslot) {
+                        $fromarray = explode(':', $timeslot->from);
+                        $toarray = explode(':', $timeslot->to);
+                        if (!in_array($array, ['fromtime' => $fromarray[0] . ":" . $fromarray[1], 'totime' => $toarray[0] . ":" . $toarray[1], 'table_id' => $timeslot->table_id, 'booked' => 0])) {
+                            array_push($array, ['fromtime' => $fromarray[0] . ":" . $fromarray[1], 'totime' => $toarray[0] . ":" . $toarray[1], 'table_id' => $timeslot->table_id, 'booked' => 0]);
                         }
                     }
-                  
                 }
 
 
