df27363bada17631a92e4c39ae2d4d5d169b724a
Eric <faisalkc4u@gmail.com>
2021-05-16 09:33:31 +0400

    changes

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -145,6 +145,12 @@ class OrderController extends Controller
                     }
                     if (isset($request->order_status)) {
                         $order->order_status = $request->order_status;
+                        if($request->order_status == 1){
+                            Detail::where('order_id', $request->id)->update(['status', 0]);
+                        }else{
+                            Detail::where('order_id', $request->id)->update(['status', 1]);
+
+                        }
                         if ($request->order_status == 2) {
                             $logistics = (new Logistics())->create($request->id);
                         }
