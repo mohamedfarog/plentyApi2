556cf83e1e4b3ebe27fa538209bfa4279f71b726
Eric <faisalkc4u@gmail.com>
2021-05-25 17:18:25 +0400

    changes

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,8 +1,41 @@
-@extends('layouts.app')
+@extends('../../layout')
 
 @section('content')
+<style>
+    .password-reset-container {
+        max-width: 700px;
+        margin: auto;
+        padding-top: 35px;
+    }
+
+    .card {
+        box-shadow: 0 1px 8px 0 rgba(0, 0, 0, 0.2);
+        transition: 0.3s;
+        padding: 10px 20px;
+        margin: 10px;
+        border-radius: 30px;
+        background-color: #fff;
+    }
+
+    .card-body {
+        max-width: 400px;
+        margin: auto;
+        padding-top: 40px;
+    }
+
+    .passreset-button {
+        margin: auto;
+        display: block;
+    }
+
+    .card-header {
+        text-align: center;
+        font-size: 18px;
+        margin-top: 45px;
+    }
+</style>
 <div class="container">
-    <div class="row justify-content-center">
+    <div class="password-reset-container">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Reset Password') }}</div>
