7846218876b25e21de6908de30e923f7892e34ab
Eric <faisalkc4u@gmail.com>
2021-05-27 16:38:46 +0400

    changes

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -41,4 +41,9 @@ class Product extends Model
     {
         return $this->hasMany(Image::class);
     }
+
+    public function tags()
+    {
+        return $this->hasMany(Productag::class);
+    }
 }
