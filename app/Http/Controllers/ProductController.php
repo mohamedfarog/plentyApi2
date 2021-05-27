95fe62e0974c8ca35058ac0e6860aff16090581f
Eric <faisalkc4u@gmail.com>
2021-05-27 16:39:16 +0400

    changes

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -66,7 +66,7 @@ class ProductController extends Controller
                             return response()->json(['success' => false, 'message' => "You dont't have enough perimission to access the data",], 400);
                         return Product::where('deleted_at', null)->where("designer_id", $designer->id)->where('shop_id',12)->with(['sizes' => function ($sizes) {
                             return $sizes->with(['color']);
-                        }, 'colors', 'addons', 'images', 'designer'])->paginate();
+                        }, 'colors', 'addons', 'images', 'designer', 'tags'])->paginate();
                         break;
             default:
                 # code...
