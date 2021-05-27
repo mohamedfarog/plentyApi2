392d3e64b62df36a3c155a37740540753fddc44b
Eric <faisalkc4u@gmail.com>
2021-05-27 18:43:07 +0400

    changes

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -29,7 +29,7 @@ class Product extends Model
             $productsarray = array();
             $related = array();
             foreach ($products as $key => $value) {
-                if($this->id == $key){
+                if($this->id == intval($key)){
                 array_push($related, $this);
 
                 }
