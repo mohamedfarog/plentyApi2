68bee602f1b86f8dd45dac0a890d83d00eccfbfa
Eric <faisalkc4u@gmail.com>
2021-05-27 17:55:06 +0400

    changes

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -789,7 +789,9 @@ class ProductController extends Controller
         if (isset($request->order) && $request->order == "asc") {
             $sortOrder = "asc";
         }
-        $product = Product::where('deleted_at', null)->where("stocks", ">", 0)->with(['sizes', 'colors', 'addons', 'images', 'designer', 'tags:id']);
+        $product = Product::where('deleted_at', null)->where("stocks", ">", 0)->with(['sizes', 'colors', 'addons', 'images', 'designer', 'tags'=>function($tags){
+                            return $tags->with(['tag:tag']);
+                        }]);
 
         if (isset($request->delete)) {
 
